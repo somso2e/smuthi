683669f0c371851d3b28c3e7c2e6b7e3c4016249
Lorenzo Pattelli <pattelli@lens.unifi.it>
2020-05-30 19:18:55 +0200

    proposal for default angular arrays. implementation still missing

--- smuthi/simulation.py
+++ smuthi/simulation.py
@@ -2,7 +2,7 @@
 """Provide class to manage a simulation."""
 
 import smuthi.linearsystem.linear_system as lsys
-import smuthi.fields
+import smuthi.fields as flds
 import sys
 import os
 import datetime
@@ -77,6 +77,7 @@ class Simulation:
                  particle_list=None,
                  initial_field=None,
                  k_parallel='default',
+                 angular_resolution=1.0,
                  neff_waypoints=None,
                  neff_imag=1e-2,
                  neff_max=None,
@@ -102,6 +103,7 @@ class Simulation:
         self.particle_list = particle_list
         self.initial_field = initial_field
         self.k_parallel = k_parallel
+        self.angular_resolution- = angular_resolution
         self.neff_waypoints = neff_waypoints
         self.neff_imag = neff_imag
         self.neff_max = neff_max
@@ -155,19 +157,19 @@ class Simulation:
     def __getstate__(self):
         """Return state values to be pickled."""
         return (self.layer_system, self.particle_list, self.initial_field, self.k_parallel, self.solver_type,
-                self.solver_tolerance, self.store_coupling_matrix, self.coupling_matrix_lookup_resolution, 
+                self.solver_tolerance, self.store_coupling_matrix, self.coupling_matrix_lookup_resolution,
                 self.coupling_matrix_interpolator_kind, self.post_processing, self.length_unit, self.save_after_run,
-                smuthi.fields.default_Sommerfeld_k_parallel_array, smuthi.fields.default_initial_field_k_parallel_array,
-                smuthi.fields.default_polar_angles, smuthi.fields.default_azimuthal_angles)
+                flds.default_Sommerfeld_k_parallel_array, flds.default_initial_field_k_parallel_array,
+                flds.default_polar_angles, flds.default_azimuthal_angles)
 
     def __setstate__(self, state):
         """Restore state from the unpickled state values."""
         (self.layer_system, self.particle_list, self.initial_field, self.k_parallel, self.solver_type,
          self.solver_tolerance, self.store_coupling_matrix, self.coupling_matrix_lookup_resolution,
          self.coupling_matrix_interpolator_kind, self.post_processing, self.length_unit, self.save_after_run,
-         smuthi.fields.default_Sommerfeld_k_parallel_array, smuthi.fields.default_initial_field_k_parallel_array,
-         smuthi.fields.default_polar_angles, smuthi.fields.default_azimuthal_angles) = state
-        
+         flds.default_Sommerfeld_k_parallel_array, flds.default_initial_field_k_parallel_array,
+         flds.default_polar_angles, flds.default_azimuthal_angles) = state
+
     def print_simulation_header(self):
         smuthi.print_smuthi_header()
         sys.stdout.write("Starting simulation.\n")
@@ -187,14 +189,14 @@ class Simulation:
     def initialize_linear_system(self):
         self.linear_system = lsys.LinearSystem(particle_list=self.particle_list,
                                                initial_field=self.initial_field,
-                                               layer_system=self.layer_system, 
+                                               layer_system=self.layer_system,
                                                k_parallel=self.k_parallel,
-                                               solver_type=self.solver_type, 
+                                               solver_type=self.solver_type,
                                                solver_tolerance=self.solver_tolerance,
                                                store_coupling_matrix=self.store_coupling_matrix,
                                                coupling_matrix_lookup_resolution=self.coupling_matrix_lookup_resolution,
                                                interpolator_kind=self.coupling_matrix_interpolator_kind)
-    
+
     def circumscribing_spheres_disjoint(self):
         """Check if all circumscribing spheres are disjoint"""
         from scipy.spatial.distance import pdist, cdist, squareform
@@ -223,7 +225,7 @@ class Simulation:
     def set_default_Sommerfeld_contour(self):
         """Set the default Sommerfeld k_parallel array"""
 
-        smuthi.fields.default_Sommerfeld_k_parallel_array = smuthi.fields.reasonable_Sommerfeld_kpar_contour(
+        flds.default_Sommerfeld_k_parallel_array = flds.reasonable_Sommerfeld_kpar_contour(
             vacuum_wavelength=self.initial_field.vacuum_wavelength,
             neff_waypoints=self.neff_waypoints,
             layer_refractive_indices=self.layer_system.refractive_indices,
@@ -232,6 +234,12 @@ class Simulation:
             neff_max_offset=self.neff_max_offset,
             neff_resolution=self.neff_resolution,
             neff_minimal_branchpoint_distance=self.neff_minimal_branchpoint_distance)
+
+    def set_default_angles(self):
+        """Set the default polar and azimuthal angular arrays for pre-processing (i.e., initial field expansion)"""
+
+        flds.default_azimuthal_angles, flds.default_polar_angles =
+        flds.angular_arrays(angular_resolution=self.angular_resolution)
 
     def set_default_initial_field_contour(self):
         """Set the default initial field k_parallel array"""
@@ -243,7 +251,7 @@ class Simulation:
             else:
                 neff_max = self.layer_system.refractive_indices[-1].real
 
-            smuthi.fields.default_initial_field_k_parallel_array = smuthi.fields.reasonable_Sommerfeld_kpar_contour(
+            flds.default_initial_field_k_parallel_array = flds.reasonable_Sommerfeld_kpar_contour(
                 vacuum_wavelength=self.initial_field.vacuum_wavelength,
                 layer_refractive_indices=self.layer_system.refractive_indices,
                 neff_imag=0,
@@ -253,7 +261,7 @@ class Simulation:
         else:
             # case of dipoles etc ...
             # use a similar contour as for Sommerfeld integrals
-            smuthi.fields.default_initial_field_k_parallel_array = smuthi.fields.reasonable_Sommerfeld_kpar_contour(
+            flds.default_initial_field_k_parallel_array = flds.reasonable_Sommerfeld_kpar_contour(
                 vacuum_wavelength=self.initial_field.vacuum_wavelength,
                 neff_waypoints=self.neff_waypoints,
                 layer_refractive_indices=self.layer_system.refractive_indices,
@@ -276,11 +284,15 @@ class Simulation:
         if self.check_circumscribing_spheres and not self.circumscribing_spheres_disjoint():
             warnings.warn("Particles with circumscribing spheres detected.")
 
+        # set default angular arrays
+        if flds.default_azimuthal_angles is None or flds.default_polar_angles is None:
+            self.set_default_angles()
+
         # check if default contours exists, otherwise set them
-        if smuthi.fields.default_Sommerfeld_k_parallel_array is None or self.overwrite_default_contours:
+        if flds.default_Sommerfeld_k_parallel_array is None or self.overwrite_default_contours:
             self.set_default_Sommerfeld_contour()
 
-        if smuthi.fields.default_initial_field_k_parallel_array is None or self.overwrite_default_contours:
+        if flds.default_initial_field_k_parallel_array is None or self.overwrite_default_contours:
             self.set_default_initial_field_contour()
 
         # prepare and solve linear system
@@ -304,10 +316,10 @@ class Simulation:
 
         if self.save_after_run:
             self.save(self.output_dir + '/simulation.p')
-        
+
         sys.stdout.write('\n')
         sys.stdout.flush()
-            
+
         #plt.show()
 
         return preparation_time, solution_time, postprocessing_time
