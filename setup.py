e424264546a1e0cca1feb46d4b42b78ed16756e4
Amos Egel <amos.egel@kit.edu>
2020-11-30 18:40:49 +0100

    tidy up setup.py, fix bug with regard to installation in develop mode

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@
 import os
 from setuptools import setup
 from setuptools.command.install import install
+from setuptools.command.develop import develop
 import pkg_resources
 import os
 import subprocess
@@ -18,56 +19,72 @@ with open("smuthi/version.py") as fp:
 __version__ = version['__version__']
 
 
-class CustomInstallCommand(install):
-    """Compile nfmds code."""
+class CustomDevelopCommand(develop):
     def run(self):
-        #install.run(self)
-        # if Windows: try to install pywigxjpf now
-        currdir = os.getcwd()
-        if sys.platform.startswith('win'):
-            sys.stdout.write('Try to install pywigxjpf ... \n')
-            sys.stdout.flush()
-            try:
-                subprocess.check_call([sys.executable, "-m", "pip", "install", "pywigxjpf"], stdout=None, stderr=None)
-            except Exception as e:
-                warnings.warn('\n*****************************************************\n'
-                              'pywigxjpf installation failed.\n'
-                              'If you want to benefit from fast Wigner3j calculations:\n'
-                              'Try to install manually by "pip install pywigxjpf".'
-                              '\n*****************************************************\n',
-                              UserWarning)                
-        
-        # compile nfmds if not built on readthedocs            
-        if not os.environ.get('READTHEDOCS'):
-            nfmds_sources_dirname = pkg_resources.resource_filename('smuthi.linearsystem.tmatrix.nfmds', 'NFM-DS')
-            sys.stdout.write('\nCompiling sources at ' + nfmds_sources_dirname + ' ...')
-            sys.stdout.flush()
-            os.chdir(nfmds_sources_dirname + '/TMATSOURCES')
-            
-            sys.stdout.flush()
-            try:
-                if sys.platform.startswith('win'):                    
-                    import encoding_converter
-                    os.chdir('win')
-                    subprocess.check_call(['f2py' ,'-c', '--compiler=mingw32', '--fcompiler=gnu95', 'TAXSYM_SMUTHI.f90', '-m', 'nfmds'])                
-                    for file in glob.glob(r'nfmds*'):
-                        shutil.move(file,'../../../'+file)
-                else:
-                    subprocess.check_call(['f2py' ,'-c', 'TAXSYM_SMUTHI.f90', '-m', 'nfmds'])
-                    for file in glob.glob(r'nfmds*'):
-                        shutil.move(file,'../../'+file)
-                sys.stdout.write(' done.\n')
-                sys.stdout.flush()
-            except Exception as e:
-                raise NameError('Compiling failed.')                
-        os.chdir(currdir)
+        install_pywigxjpf()
+        compile_nfmds()
+        develop.run(self)
+
+
+class CustomInstallCommand(install):
+    def run(self):
+        install_pywigxjpf()
+        compile_nfmds()
         install.run(self)
+
+
+def install_pywigxjpf():
+    """If Windows: try to install pywigxjpf"""
+    if sys.platform.startswith('win'):
+        sys.stdout.write('Try to install pywigxjpf ... \n')
+        sys.stdout.flush()
+        try:
+            subprocess.check_call([sys.executable, "-m", "pip", "install", "pywigxjpf"], stdout=None, stderr=None)
+        except Exception as e:
+            warnings.warn('\n*****************************************************\n'
+                          'pywigxjpf installation failed.\n'
+                          'If you want to benefit from fast Wigner3j calculations:\n'
+                          'Try to install manually by "pip install pywigxjpf".'
+                          '\n*****************************************************\n',
+                          UserWarning)
+
+
+def compile_nfmds():
+    """compile nfmds Fortran extension (unless built on readthedocs)"""
+    currdir = os.getcwd()
+    if not os.environ.get('READTHEDOCS'):
+        nfmds_sources_dirname = pkg_resources.resource_filename('smuthi.linearsystem.tmatrix.nfmds', 'NFM-DS')
+        sys.stdout.write('\nCompiling sources at ' + nfmds_sources_dirname + ' ...')
+        sys.stdout.flush()
+        os.chdir(nfmds_sources_dirname + '/TMATSOURCES')
+
+        sys.stdout.flush()
+        try:
+            if sys.platform.startswith('win'):
+                import encoding_converter
+                os.chdir('win')
+                subprocess.check_call(
+                    ['f2py', '-c', '--compiler=mingw32', '--fcompiler=gnu95', 'TAXSYM_SMUTHI.f90', '-m', 'nfmds'])
+                for file in glob.glob(r'nfmds*'):
+                    shutil.move(file, '../../../' + file)
+            else:
+                subprocess.check_call(['f2py', '-c', 'TAXSYM_SMUTHI.f90', '-m', 'nfmds'])
+                for file in glob.glob(r'nfmds*'):
+                    shutil.move(file, '../../' + file)
+            sys.stdout.write(' done.\n')
+            sys.stdout.flush()
+        except Exception as e:
+            raise NameError('Compiling failed.')
+    os.chdir(currdir)
+
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
 def get_requirements():
+    """Return a list of requirements. If windows, don't include pywigxjpf, because we want to have it as an optional
+    dependency in that case (will be taken care of in custom install command)"""
     requirements = ['argparse',
                     'imageio',
                     'matplotlib',
@@ -106,7 +123,7 @@ setup(
               'smuthi.postprocessing',
               'smuthi.utility'],
     cmdclass={'install': CustomInstallCommand,
-              'develop': CustomInstallCommand},
+              'develop': CustomDevelopCommand},
     package_data={'smuthi.linearsystem.tmatrix.nfmds': ['NFM-DS/*.txt', 'NFM-DS/TMATSOURCES/*.f90', 'NFM-DS/TMATFILES/*',
                                                         'NFM-DS/INPUTFILES/*.dat', 'NFM-DS/OUTPUTFILES/*','nfmds*'],
                   'smuthi': ['_data/*']},
@@ -117,4 +134,3 @@ setup(
     entry_points={'console_scripts': ['smuthi = smuthi.__main__:main']},
     license='MIT',
 )
-
