a60b38bcf6acaeced022860be00daf81c0846529
Amos Egel <ae@hembach-photonik.de>
2020-12-02 19:19:45 +0100

    fixup

--- CI/build-wheel-manylinux.sh
+++ CI/build-wheel-manylinux.sh
@@ -3,8 +3,12 @@
 # build wheel
 for PYBIN in /opt/python/*/bin/; do
 #    targetversion = 
-    if [$PYBIN == *cp27*]   continue
-		if [$PYBIN == *cp35*]   continue
-    echo "Compiling using pip version ${PYBIN}...."
+  if [$PYBIN == *cp27*] then 
+    continue 
+  fi
+  if [$PYBIN == *cp35*] then 
+    continue 
+  fi
+  echo "Compiling using pip version ${PYBIN}...."
 #    PATH=/opt/usr/bin/:${PYBIN}:${PATH}   pip wheel --no-deps ./ -w wheelhouse
 done
