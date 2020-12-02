fa0dea457bec043882b98d50fe339b41bd09b36d
Amos Egel <ae@hembach-photonik.de>
2020-12-02 19:16:37 +0100

    CI: manylinux - exclude old python versions

--- CI/build-wheel-manylinux.sh
+++ CI/build-wheel-manylinux.sh
@@ -3,7 +3,8 @@
 # build wheel
 for PYBIN in /opt/python/*/bin/; do
 #    targetversion = 
-#    if [$PYBIN == **]   continue
+    if [$PYBIN == *cp27*]   continue
+		if [$PYBIN == *cp35*]   continue
     echo "Compiling using pip version ${PYBIN}...."
 #    PATH=/opt/usr/bin/:${PYBIN}:${PATH}   pip wheel --no-deps ./ -w wheelhouse
 done
