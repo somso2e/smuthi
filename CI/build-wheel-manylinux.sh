b19417d00400baf023a993842c9cfb6e68aaeb63
Amos Egel <ae@hembach-photonik.de>
2020-12-02 19:43:42 +0100

    fixup

--- CI/build-wheel-manylinux.sh
+++ CI/build-wheel-manylinux.sh
@@ -4,9 +4,11 @@
 for PYBIN in /opt/python/*/bin/; do
 #    targetversion = 
   if [ $PYBIN == *cp27* ]; then 
+    echo "Skipping ${PYBIN}...."
     continue 
   fi
   if [ $PYBIN == *cp35* ]; then 
+    echo "Skipping ${PYBIN}...."
     continue 
   fi
   echo "Compiling using pip version ${PYBIN}...."
