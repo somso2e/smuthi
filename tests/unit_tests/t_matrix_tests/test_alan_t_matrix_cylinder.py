60be61f9f003d0bc1bdd5b6647d148964122e96b
Amos Egel <ae@hembach-photonik.de>
2021-01-04 14:43:21 +0100

    fix test

--- tests/unit_tests/t_matrix_tests/test_alan_t_matrix_cylinder.py
+++ tests/unit_tests/t_matrix_tests/test_alan_t_matrix_cylinder.py
@@ -12,33 +12,32 @@ import numpy as np
 
 # We compute the T-matrix of a cylinder with 100nm height and 50nm radius
 
-lmax = 3
-mmax = 3
-nrank = 10
-
-nmed = 1
-wl = 500
-
-axsym_cylinder = part.FiniteCylinder(position=(0, 0, 0),
-                                     refractive_index=2,
-                                     cylinder_radius=50,
-                                     cylinder_height=100,
-                                     l_max=lmax,
-                                     m_max=mmax,
-                                     n_rank=nrank)
-
-t_axsym = axsym_cylinder.compute_t_matrix(vacuum_wavelength=wl,
-                                          n_medium=nmed)
-
-axsym_cylinder.python_tmt = True
-axsym_cylinder.nrank = 10
-
-t_python = axsym_cylinder.compute_t_matrix(vacuum_wavelength=wl,
-                                           n_medium=nmed)
-
 
 def test_python_cylinder():
-    return # test_python_cylinder()  <- uncomment when Alan's code is on PyPi
+    lmax = 3
+    mmax = 3
+    nrank = 10
+
+    nmed = 1
+    wl = 500
+
+    axsym_cylinder = part.FiniteCylinder(position=(0, 0, 0),
+                                         refractive_index=2,
+                                         cylinder_radius=50,
+                                         cylinder_height=100,
+                                         l_max=lmax,
+                                         m_max=mmax,
+                                         n_rank=nrank)
+
+    t_axsym = axsym_cylinder.compute_t_matrix(vacuum_wavelength=wl,
+                                              n_medium=nmed)
+
+    axsym_cylinder.python_tmt = True
+    axsym_cylinder.nrank = 10
+
+    t_python = axsym_cylinder.compute_t_matrix(vacuum_wavelength=wl,
+                                               n_medium=nmed)
+
     print(t_axsym[0, 0])
     print(t_python[0, 0])
 
@@ -48,4 +47,5 @@ def test_python_cylinder():
 
 
 if __name__ == '__main__':
-    test_python_cylinder()
+    pass
+    # test_python_cylinder()
