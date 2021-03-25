2c5298f997cfaa76e3d3fa9ae041d83047ebc99c
Amos Egel <ae@hembach-photonik.de>
2021-03-25 10:13:53 +0100

    numpy 1.17 failed, try 1.19

--- setup.py
+++ setup.py
@@ -94,9 +94,9 @@ def get_requirements():
     #                with numpy version 1.12 and that 
     #                user machines have at least 1.12
     if os.environ.get('CI'):
-        requirements.append('numpy==1.17')
+        requirements.append('numpy==1.19')
     else: 
-        requirements.append('numpy>=1.17')
+        requirements.append('numpy>=1.19')
         
     return requirements
 
